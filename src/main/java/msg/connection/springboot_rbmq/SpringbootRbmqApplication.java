87e79a9f749af7d0556cd0257d8b8a8649d09d3b
Snehil Anand <snehilanand512@gmail.com>
2025-10-05 18:45:55 +0530

    New Application for RabbitMq

---
+++ src/main/java/msg/connection/springboot_rbmq/SpringbootRbmqApplication.java
@@ -0,0 +1,13 @@
+package msg.connection.springboot_rbmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRbmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRbmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msg/connection/springboot_rbmq/SpringbootRbmqApplicationTests.java
@@ -0,0 +1,13 @@
+package msg.connection.springboot_rbmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRbmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
