7ba2b96f2fc79f10291f65dd52a8e07717815695
Snehil Anand <snehilanand512@gmail.com>
2025-10-06 16:20:10 +0530

    Changes2

---
+++ src/main/java/msg/connection/springboot_rbmq/controller/MessageController.java
@@ -0,0 +1,23 @@
+package msg.connection.springboot_rbmq.controller;
+
+import msg.connection.springboot_rbmq.producer.RabbitMQProd;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/send")
+public class MessageController {
+
+    @Autowired
+    private RabbitMQProd rabbitMQProducer;
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message Transferred Successfully");
+    }
+}
